a6d87f0c2a3fae7e5aa178e7e7ce0779a5cbe25f
Domi <Domi@Domi-PC.Speedport_W_724V_09011603_05_010>
2018-01-03 11:29:33 +0100

    First Lesson

---
+++ LessonOne/src/lections/BasisTypen.java
@@ -0,0 +1,31 @@
+package lections;
+
+public class BasisTypen {
+
+	public static void main(String[] args) {
+		//Übung 1
+		boolean myBoolean = false;
+		myBoolean = myBoolean || true;
+		System.out.println(myBoolean);
+		
+		//Übung 2
+		int i = 0;
+		i = i + 4;
+		System.out.println(i);
+		
+		//Übung 3
+		int j = 2147483647;
+		j = j + 1;
+		System.out.println(j);
+		
+		//Übung 4
+		int z = 22;
+		int z2 = 0;
+		System.out.println(z/z2);
+
+		//Übung 5
+
+		
+	}
+
+}
---
+++ LessonOne/src/lections/HelloWorld.java
@@ -0,0 +1,16 @@
+package lections;
+
+public class HelloWorld {
+
+	/**
+	 * Diese Methode startet das Programm
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * My first programm :)
+		 */
+		System.out.println("Hello World!"); //TODO: Auf Deutsch übersetzen
+	}
+	
+}
